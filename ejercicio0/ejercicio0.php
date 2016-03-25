98701cdd8ccd8b5b1797846ef3e1bbc95c5cc73f
Manuel Flores <manuel.flores.villatoro@ues.edu.sv>
2016-03-25 12:13:12 -0600

    ejercicio0 terminado

--- ejercicio0/ejercicio0.php
+++ ejercicio0/ejercicio0.php
@@ -8,7 +8,37 @@
 
 
 function numero_mayor(array $numeros){
-  return 10;
+
+  //Inicializamos el numero ordenado mayor
+  $numeroOrdenado='';
+
+  //Contamos cuantos numeros hay en el arreglos
+  // y le restamos uno, por se hacer n-1 comparaciones
+  $elementosComparar=count($numeros)-1;
+  
+
+  // Recorremos n-1 veces el arreglo para ordenar el arreglo
+  for ($pasada=0; $pasada<$elementosComparar;$pasada++){
+
+    // Recorremos n-1 veces el cada elemento del arreglo
+    for ($contador=0; $contador<$elementosComparar;$contador++){
+      //Si el elemento actual es menor que el siguiente, 
+      // Cambiamos el mayor al primer elemento usando una
+      // variable auxiliar
+      if($numeros[$contador][0]<$numeros[$contador+1][0]){
+        $numAux=$numeros[$contador];
+        $numeros[$contador]=$numeros[$contador+1];      
+        $numeros[$contador+1]=$numAux;      
+      }
+    }
+  }
+  //Concatenamos todos los numeros en una variable
+  foreach($numeros as $elemento){
+    $numeroOrdenado=$numeroOrdenado.$elemento;
+  }
+  //Devolvemos la variable como entero  usando casting.
+  // Por que $numeroOrdenado era tipo string.
+  return  (int) $numeroOrdenado;
 }
 
 ?>
--- ejercicio0/prueba0.php
+++ ejercicio0/prueba0.php
@@ -2,15 +2,19 @@
 require_once("ejercicio0.php");
 
 
-$numerosPrueba1= array (50,2,1,9);
-$numerosPrueba2= array (12,9,8,3);
-$numerosPrueba3= array (12,9,8,3);
-$numerosPrueba4= array (34,3,9,10);
+$numerosPrueba1= array ('50','2','1','9');
+$numerosPrueba2= array ('12','9','8','3');
+$numerosPrueba3= array ('12','9','8','3');
+$numerosPrueba4= array ('34','3','9','10');
 
 
+print_r($numerosPrueba1);
 echo "Numero mayor para prueba 1 es:".numero_mayor($numerosPrueba1)."\n";
+print_r($numerosPrueba2);
 echo "Numero mayor para prueba 2 es:".numero_mayor($numerosPrueba2)."\n";
+print_r($numerosPrueba3);
 echo "Numero mayor para prueba 3 es:".numero_mayor($numerosPrueba3)."\n";
+print_r($numerosPrueba4);
 echo "Numero mayor para prueba 4 es:".numero_mayor($numerosPrueba4)."\n";
 
 
