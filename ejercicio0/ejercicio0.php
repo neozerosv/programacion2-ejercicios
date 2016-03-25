97325954c7a14243b0509dc3b10243439cfa3de5
Manuel Flores <manuel.flores.villatoro@ues.edu.sv>
2016-03-25 10:55:50 -0600

    Ejercicio de prueba para programacion II

---
+++ ejercicio0/ejercicio0.php
@@ -0,0 +1,14 @@
+<?php
+//*****************************//
+// Ejercicio #0 
+// Manuel de Jesus Flores
+// Carnet FF000000
+// Programacion 2 ULS
+//****************************//
+
+
+function numero_mayor(array $numeros){
+  return 10;
+}
+
+?>
---
+++ ejercicio0/prueba0.php
@@ -0,0 +1,18 @@
+<?php
+require_once("ejercicio0.php");
+
+
+$numerosPrueba1= array (50,2,1,9);
+$numerosPrueba2= array (12,9,8,3);
+$numerosPrueba3= array (12,9,8,3);
+$numerosPrueba4= array (34,3,9,10);
+
+
+echo "Numero mayor para prueba 1 es:".numero_mayor($numerosPrueba1)."\n";
+echo "Numero mayor para prueba 2 es:".numero_mayor($numerosPrueba2)."\n";
+echo "Numero mayor para prueba 3 es:".numero_mayor($numerosPrueba3)."\n";
+echo "Numero mayor para prueba 4 es:".numero_mayor($numerosPrueba4)."\n";
+
+
+
+?>
